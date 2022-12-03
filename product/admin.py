6d1c8f1b2a8cd4d50dfd4bf5d8d2c4d38d2c7d6b
Karthick Selvaraj <karthick@mallow-tech.com>
2022-12-03 12:58:00 +0530

    Basic setup, product and category added

--- product/admin.py
+++ product/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from .models import Product, Category
 
-# Register your models here.
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ['name', 'category', 'created_at', 'updated_at']
+    search_fields = ['name']
+
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ['name', 'created_at', 'updated_at']
+    search_fields = ['name']
--- product/models.py
+++ product/models.py
@@ -1,3 +1,23 @@
 from django.db import models
 
-# Create your models here.
+class TimeMixin(models.Model):
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    deleted_at = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        abstract = True
+        
+class Category(TimeMixin):
+    name = models.CharField(max_length=200)
+
+    def __str__(self) -> str:
+        return self.name
+
+class Product(TimeMixin):
+    name = models.CharField(max_length=200)
+    category = models.ForeignKey(Category, on_delete=models.SET_NULL, null=True)
+
+    def __str__(self) -> str:
+        return self.name
+
